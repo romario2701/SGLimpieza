b66ec2c8dff2f7871a0aed6f89af2843c216ce70
VERNAZA CAÑOLA ROMARIO JOEL <144184994+romario2701@users.noreply.github.com>
2025-10-20 22:40:20 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "gestion-limpieza-mercado",
+  "version": "1.0.0",
+  "description": "Programa de gestión de limpieza de un mercado municipal",
+  "main": "index.js",
+  "scripts": {
+    "test": "node tests/ejemplo.test.js"
+  },
+  "dependencies": {},
+  "devDependencies": {}
+}
